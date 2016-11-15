a6495395cba5af5416be8f1c1349da1cb1c79efc
Chase Skelton <redghost50@gmail.com>
2016-11-14 23:31:45 -0800

    now using BigInteger

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,37 +1,41 @@
 import java.util.Scanner;
+import java.math.BigInteger;
 import java.util.TreeMap;
 
 public class Fibonacci {
 	public static void main(String args[]){
 		Scanner input = new Scanner(System.in);
 		System.out.println("Please enter the position of the Fibonacci number (zero base):");
-		TreeMap<Integer, Double> map = new TreeMap<>();
-		map.put(0,0.0);
-		map.put(1,1.0);
+		TreeMap<Integer, BigInteger> map = new TreeMap<>();
+		BigInteger a = new BigInteger("0");
+		BigInteger b = new BigInteger("1");
+		map.put(0,a);
+		map.put(1,b);
 		
 		while(true){
 			int inputInt = input.nextInt();
 			if(inputInt < 0){
 				break;
 			}
-			double outputInt = 0;
+			BigInteger outputInt = new BigInteger("0");
 			if(map.containsKey(inputInt)){
-				outputInt = (double)map.get((inputInt));
+				outputInt = map.get(inputInt);
 			}
 			else{
-				outputInt = (double)calculateFib(inputInt, map);
+				outputInt = calculateFib(inputInt, map);
 			}
 			System.out.println(outputInt);
+			System.out.println("Number of digits: " + outputInt.toString().length());
 		}
 		input.close();
 	}
 	
-	public static double calculateFib(int place, TreeMap<Integer,Double> map){
+	public static BigInteger calculateFib(int place, TreeMap<Integer,BigInteger> map){
 		if(map.containsKey(place)){
-			return (double)map.get(place);
+			return map.get(place);
 		}
 		else{
-			map.put(place, (calculateFib((place - 1), map) + calculateFib((place -2), map)));
+			map.put(place, (calculateFib((place - 1), map).add( calculateFib((place -2), map))));
 			return map.get(place);
 		}
 	}
